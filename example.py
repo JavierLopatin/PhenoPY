3006d07d5d9b55ab2da387204ef3f607095c6388
JavierLopatin <javierlopatin@gmail.com>
2019-06-11 17:39:42 +0200

    finish pehnoplot

--- example.py
+++ example.py
@@ -1,19 +1,23 @@
-import os
+
 import pandas as pd
 import phenopy as phen
 import timeit
-# in data
-os.chdir('/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta')
-days = 'dates.txt'
-inData = 'rasterTest.tif'
+import matplotlib.pyplot as plt
+
+days = '/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta/dates.txt'
+inData = '/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta/rasterTest.tif'
 inData2 = '/home/javier/Documents/SF_delta/Sentinel/TSA/X-004_Y-001/2015-2019_001-365_LEVEL4_TSA_SEN2L_EVI_C0_S0_FAVG_TY_C95T_TSS.tif'
+outData = '/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta/outTest.tif'
 # import dates
 dates = pd.read_csv(days, header=None)[0]
 dates = pd.to_datetime(dates)
 # create dummy set of dates all with same year to mix according to DOY
 #years2 = years.apply(lambda dt: dt.replace(year=2018))
 
-start = timeit.timeit()
-phen.PhenoShape(inData, 'test.tif', dates=dates, rollWindow=3, nGS=46, nan_replace=-32767)
-end  = timeit.timeit()
-print(end - start)
+# Plot
+X = 579470.599
+Y = 4224782.793
+outFigure = '/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta/testFig.pdf'
+phen.PhenoPlot(X=X, Y=Y, inData=inData, dates=dates, saveFigure=outFigure, ylim=None, rollWindow=5, nGS=46, ylab='EVI')
+
+phen.PhenoShape(inData=inData, saveRaster=outData, dates=dates, rollWindow=5, nGS=46, nan_replace=-32767)
--- phenopy.py
+++ phenopy.py
@@ -10,89 +10,119 @@ import rasterio
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import xarray as xr
+from numba import jit
 
-X = 579470.599
-Y = 4224782.793
-phenoshape = '/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta/test.tif'
-tss = '/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta/rasterTest.tif'
-time = '/home/javier/Documents/SF_delta/Sentinel/TSA/npphen_consulta/dates.txt'
-nGS = 46
-ylab = 'EVI'
+def PhenoPlot(X, Y, inData, dates, saveFigure=None, ylim=None, rollWindow=None,
+              nGS=46, ylab='NDVI'):
+    """
+    Plot the PhenoShape curve along with the yearly data
 
-# get spatial point
-point = Point(X, Y)
-# get pixel value per pixel
-# first read metadata to get number of bands
-with rasterio.open(tss) as r:
-    countTSS = r.count
+    Args:
+        - X: X coordinates
+        - Y: Y coordinates
+        - inData: Absolute path to the original timeseries data
+        - dates: Dates of the original timeseries data [dtype: datetime64[ns]]
+        - saveFigure: Absolute path with extention to save figure on disk
+        - ylim: Limits of the Y axis [default the y min() and max() values]
+        - rollWindow: integer with value of avarage smoothing of linear trend [default None]
+        - nGS: Number of observations to predict the PhenoShape [default 46: one per week]
+        - ylab: Label of the Y axis [default "NDVI"]
 
-with rasterio.open(phenoshape) as r:
-    countShape = r.count
+    """
+    # get spatial point
+    point = Point(X, Y)
+    # get pixel value per pixel
+    # first read metadata to get number of bands
+    with rasterio.open(inData) as r:
+        countTSS = r.count
 
-# save values
-valuesTSS = []
-valuesShape = []
+    # save values
+    valuesTSS = []
+    for i in range(countTSS):
+        valuesTSS.append(point_query(point, inData, band=(i+1)))
+    valuesTSS = np.array(valuesTSS, dtype=np.float).squeeze()
 
-for i in range(countTSS):
-    valuesTSS.append(point_query(point, tss, band=(i+1)))
-valuesTSS = np.array(valuesTSS).squeeze()
+    # load dates
+    valuesTSSpd = pd.concat([pd.DataFrame(dates),
+                             pd.DataFrame(dates.dt.dayofyear),
+                             pd.DataFrame(dates.dt.year),
+                             pd.DataFrame(valuesTSS)], axis=1).dropna()
+    valuesTSSpd.columns = ['dates', 'doy', 'year', 'VI']
 
-for i in range(countTSS):
-    valuesShape.append(point_query(point, phenoshape, band=(i+1)))
-valuesShape = np.array(valuesShape).squeeze()
+    # group values according to year
+    groups = valuesTSSpd.groupby('year')
 
-# load dates
-dates = pd.read_csv(time, header=None)[0]
-dates = pd.to_datetime(dates)
-valuesTSSpd = pd.concat([pd.DataFrame(dates),
-                         pd.DataFrame(dates.dt.dayofyear),
-                         pd.DataFrame(dates.dt.year),
-                         pd.DataFrame(valuesTSS)], axis=1).dropna()
-valuesTSSpd.columns = ['dates', 'doy', 'year', 'VI']
+    # Transform numpy array to xarray
+    xarray = xr.DataArray(valuesTSS)
+    # DOY coordinates
+    xarray.coords['doy'] = dates.dt.dayofyear
+    # sort values by DOY
+    xarray = xarray.sortby('doy')
+    # rearrange time dimension for smoothing
+    xarray['dim_0'] = xarray['doy']
+    # rolling average using moving window
+    if rollWindow is not None:
+        xarray = xarray.rolling(dim_0=rollWindow, center=True).mean()
 
-# how many years
-years = dates.dt.year.unique()
+    # predict linear interpolation
+    @jit
+    def getPheno(y, x):
+        """
+        Apply linear interpolation in the 'time' axis
+        x: DOY values
+        y: ndarray with VI values
+        """
+        inds = np.isnan(y)  # check if array has NaN values
+        if np.sum(inds) == len(x):  # check is all values are NaN
+            print('ERROR: All values are NaN')
+        else:
+            if inds.any():  # if inds have at least one True
+                x = x[~inds]
+                y = y[~inds]
+                xnew = np.linspace(np.min(x), np.max(x), nGS, dtype='int16')
+                ynew = np.interp(xnew, x, y)
 
-groups = valuesTSSpd.groupby('year')
+            else:
+                xnew = np.linspace(np.min(x), np.max(x), nGS, dtype='int16')
+                ynew = np.interp(xnew, x, y)
 
-fig, ax = plt.subplots()
-ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
-for name, group in groups:
-    ax.plot(group.doy, group.VI, marker='o', linestyle='', ms=12, label=name)
-ax.legend()
-plt.ylab(ylab)
-plt.xlab('Day of the year')
-plt.show()
+            return xnew, ynew
 
-plt.plot(valuesTSSpd['doy'], valuesTSSpd['VI'], 'o')
-valuesTSSpd.plot.scatter(x='doy',
-                y='VI',
-                c='year',
-                colormap='viridis')
+    # get phenology shape accross the time axis
+    y = xarray.values
+    x = xarray.doy.values
+    xnew, phen = getPheno(y, x)
 
-newx = np.linspace(np.min(x), np.max(x), nGS, dtype='int16')
-
-
-
+    # plot
+    for name, group in groups:
+        plt.plot(group.doy, group.VI, marker='o', linestyle='', ms=10, label=name)
+    plt.plot(xnew, phen, '-', color='black')
+    plt.legend()
+    if ylim is not None:
+        plt.ylim(ylim[0], ylim[1])
+    plt.ylabel(ylab, fontsize=14)
+    plt.xlabel('Day of the year', fontsize=14)
+    if saveFigure is not None:
+        plt.savefig(saveFigure)
+    plt.show()
 
 
 def PhenoShape(inData, saveRaster, dates=None, nan_replace=None,
                rollWindow=None, nGS=46, chuckSize=256):
     """
-    **summary line** Process phenological shape of remote sensing data by
+    Process phenological shape of remote sensing data by
     folding data to day-of-the-year. Process is done in a block-by-block way
     with parallel processing.
 
-    **description starts after blank line above**
     Args:
-        - inDara:...
+        - inData: Absolute path to the original timeseries data
+        - dates: Dates of the original timeseries data [dtype: datetime64[ns]]
+        - saveRaster: Absolute path with extention to save raster on disk
+        - ylim: Limits of the Y axis [default the y min() and max() values]
+        - rollWindow: integer with value of avarage smoothing of linear trend [default None]
+        - nGS: Number of observations to predict the PhenoShape [default 46: one per week]
     """
-    import xarray as xr
-    import numpy as np
-    from numba import jit
-    import rasterio
-    from dash import ProgressBar
-
     # get variables
     try:
         # load raster as a xarray
@@ -106,21 +136,22 @@ def PhenoShape(inData, saveRaster, dates=None, nan_replace=None,
         attrs = xarray.attrs
         # add day of the year coordinates
         xarray.coords['doy'] = xarray.time.dt.dayofyear
-    except RasterioIOError:
+    except TypeError:
         print('ERROR: data must be a GDAL format')
 
     # sort basds according to day-of-the-year
     xarray = xarray.sortby('doy')
+    # rearrange time dimension for smoothing and interpolation
+    xarray['time'] = xarray['doy']
     # turn a value to NaN
     if nan_replace is not None:
         xarray = xarray.where(xarray.values != nan_replace)
     # rolling average using moving window
     if rollWindow is not None:
         xarray = xarray.rolling(time=rollWindow, center=True).mean()
-
     # prepare inputs to getPheno
     x = xarray.doy.values
-    y = xarray.values  # [:,100,100]
+    y = xarray.values
 
     @jit
     def getPheno(y, x):
@@ -130,6 +161,7 @@ def PhenoShape(inData, saveRaster, dates=None, nan_replace=None,
         y: ndarray with VI values
         """
         inds = np.isnan(y)  # check if array has NaN values
+        y.dtype
         if np.sum(inds) == len(x):  # check is all values are NaN
             return y[0:nGS]
         else:
@@ -147,18 +179,21 @@ def PhenoShape(inData, saveRaster, dates=None, nan_replace=None,
 
     # get phenology shape accross the time axis
     phen = np.apply_along_axis(getPheno, 0, y, x)
+    xnew = np.linspace(np.min(x), np.max(x), nGS, dtype='int16')
     # add phenology into an xarray format according to the input xarray
     phen = xr.DataArray(phen, dims=xarray.dims, coords={
                         'time': np.linspace(np.min(x), np.max(x), nGS, dtype='int16'),
                         'y': xarray.coords['y'],
                         'x': xarray.coords['x']},
                         attrs=attrs)
-    # phen[10,:,:].plot()
-
-    # save to disk
-
-    # edit metadata before save the raster
-    meta.update(count=nGS, dtype=phen.values.dtype)
+        # edit metadata before save the raster
+    meta.update(count=nGS, dtype=phen.values.dtype, nodata=np.nan)
     # save results
     with rasterio.open(saveRaster, "w", **meta) as dst:
         dst.write(phen)
+        # save band description to metadata
+        for i in range(len(xnew)):
+            dst.set_band_description(i+1, 'DOY '+str(xnew[i]))
+
+
+min = np.min(phen)
