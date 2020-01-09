73f588dcc9070d8f47b12c8aba5b3ca1d6e5c89c
JavierLopatin <javierlopatin@gmail.com>
2020-01-09 18:38:48 -0300

    beautify

--- example.py
+++ example.py
@@ -11,13 +11,13 @@ doy = dates.dt.dayofyear
 doy
 
 # Plot
-X = 581309.34378
-Y = 4224422.50636
-phen.PhenoPlot(X=X, Y=Y, inData=inData, dates=dates, type='KDE', plotType=1,
-               rollWindow=5, ylab='EVI')
+X = 583515.162
+Y = 4231631.138
+phen.PhenoPlot(X=X, Y=Y, inData=inData, dates=dates, plotType=1,
+          rollWindow=5, ylab='EVI')
 
-phen.PhenoPlot(X=X, Y=Y, inData=inData, dates=dates, type='KDE', plotType=2,
-               rollWindow=5, n_phen=10, ylab='EVI')
+phen.PhenoPlot(X=X, Y=Y, inData=inData, dates=dates, plotType=2, phentype=2,
+          rollWindow=5, n_phen=10, ylab='EVI')
 
 # produce shape of phenology
 inData = '/home/javier/Documents/SF_delta/Sentinel/npphen_consulta/rasterTest.tif'
@@ -38,12 +38,18 @@ with rasterio.open(outData) as r:
     phen = r.read()
 
 # get phenological shape of the wetlands
-phen.PhenoShape(inData=inData, outData=outData, dates=dates, rollWindow=5,
-                nan_replace=-32767, nGS=46, chuckSize=16, n_jobs=3)
+PhenoShape(inData=inData, outData=outData, dates=dates, rollWindow=5,
+           nan_replace=-32767, nGS=46, chuckSize=16, n_jobs=3)
 
 # get land surface phenology metrics
-phen.PhenoLSP(inData=outData, outData=outData[:-4]+'_LSP.tif', doy=doy,
-              nGS=46, n_jobs=8, chuckSize=16)
+PhenoLSP(inData=outData, outData=outData[:-4]+'_LSP2.tif', doy=doy, phentype=2,
+         nGS=46, n_jobs=8, chuckSize=16)
 
 # get RMSE between the fitted phenoshape and the real distribution of values
-phen.RMSE(inData, outData, outData[:-4]+'_RMSE.tif', dates)
+RMSE(inData, outData, outData[:-4]+'_RMSE.tif', dates)
+
+
+
+PhenoLSP(inData='/home/javier/Documents/SF_delta/Sentinel/LSP/X-004_Y-001_phenoshape.tif',
+         outData='/home/javier/Documents/SF_delta/Sentinel/LSP/X-004_Y-001_LSP2.tif', doy=doy, phentype=1,
+         nGS=46, n_jobs=8, chuckSize=128)
